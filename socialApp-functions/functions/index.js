cfd0864def52f02352a8238e26652073bbcd5c9a
Nando Linhares <fernandolinhares1920@globo.com>
2019-08-19 20:36:06 -0300

    Validating my signup form backend

--- socialApp-functions/functions/index.js
+++ socialApp-functions/functions/index.js
@@ -64,6 +64,21 @@ app.post('/scream',(req, res) => {
 });
 
 // Boa prática http://baseurl.com/api/route...
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false;
+}
+
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) {
+        return true;
+    } else {
+        return false; 
+    }
+}
+
 //SignUp route
 app.post('/signup', (req, res) => {
     const newUser = {
@@ -72,6 +87,22 @@ app.post('/signup', (req, res) => {
         confirmPassword: req.body.confirmPassword,
         handle: req.body.handle
     }
+
+    let errors = {}; // Um objeto
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty';
+    } else if(!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address';
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty';
+    if(newUser.password !== newUser.confirmPassword) 
+        errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    // Verificar se algum erro ainda está salvo no array, para continuar
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
 
     // Todo: Validate data
     let token, userId;
